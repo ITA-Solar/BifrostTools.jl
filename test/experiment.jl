b83397d2bb64c4e03087887b46516c2063c6b098
meudnaes <eliasudnes@hotmail.com>
2024-06-13 12:49:03 +0200

    [DEV] including utils test, small change in experiment test

--- test/experiment.jl
+++ test/experiment.jl
@@ -1,6 +1,9 @@
 @testset "experiment and mesh" begin
     
     @test xp.snaps == [1]
-    @test xp.snapsize == (xp.mesh.mx, xp.mesh.my, xp.mesh.mz)
+    # (nx, ny, nz)
+    @test xp.snapsize == BifrostTools.get_snapsize(xp.mesh) == (48, 48, 64)
+    # 8 primary, 2 aux
+    @test xp.num_primary_vars == 8
 
 end
