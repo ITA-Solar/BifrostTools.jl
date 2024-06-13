2e8ce91d1042e7466b5589ad34ec1ec2a9ae292c
meudnaes <eliasudnes@hotmail.com>
2024-06-13 09:31:18 +0200

    [ADD] testing eos_tables with electron density

---
+++ test/eos_tables.jl
@@ -0,0 +1,16 @@
+@testset "calculate electron density" begin
+    
+    tmp_array = Float32[
+        3.049999f14, 3.0051822f14, 2.9655743f14, 2.9357018f14, 2.9122838f14, 
+        2.8965733f14, 2.8885182f14, 2.885963f14, 2.8863922f14, 2.891092f14, 
+        2.904146f14, 2.929381f14, 2.9665926f14, 3.0098403f14, 3.0582764f14, 
+        3.110285f14, 3.1600604f14, 3.207982f14, 3.2572512f14, 3.3144383f14
+    ]
+
+
+    ne = get_electron_density(expname,xp.snaps[1],expdir,
+        slicex=[10],slicey=5:24,slicez=[10])
+
+    @test ne[1,:,1] == tmp_array
+
+end
