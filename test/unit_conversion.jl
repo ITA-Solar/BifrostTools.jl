a0fe85268d0617c9d8ffb56a9ae0ee9aca53a0f1
eilifso <e.s.oyre@astro.uio.no>
2024-06-17 12:03:40 +0200

    [DEV] Use copy of cgs-to-SI factors in test to detect changes in src.

--- test/unit_conversion.jl
+++ test/unit_conversion.jl
@@ -37,6 +37,41 @@ u_B =  1.121e3
         "qediff" => u_e / u_t,
         "qeadv" => u_e / u_t
     )
+
+    test_cgs_to_si_conversions = Dict(
+    # Pressure:    g/s^2/cm * 1f-3 kg/g * 1f2 cm/m = 1f-1 kg/s^2/m
+    "p"  => 1e-1,
+    # Gas density: g/cm^3 * 1f-3 kg/g * 1f6 cm^3/m^3 = 1f3 kg/m^3
+    "r"  => 1e3,
+    # Momentum:    g/cm^2/s * 1f-3 kg/g * 1f4 cm^2/m^2 = 1f1 kg/m^2/s
+    "px" => 1e1,
+    "py" => 1e1,
+    "pz" => 1e1,
+    # Bulk velocity:  cm/s * 1f-2 m/cm = 1f-2 m/s
+    "ux" => 1e-2,
+    "uy" => 1e-2,
+    "uz" => 1e-2,
+    # Internal energy:     erg/cm^3 * 1f-7 J/erg * 1f6 cm^3/m = 1f-1 J/m^3
+    "e"  => 1e-1,
+    # Dissipation coefficients/energy terms:     erg/cm^3/s * 1.e-7 J/erg * 1.e6 cm^3/m^3 = W/m^3
+    "qvisc" => 1e-1,
+    "qjoule" => 1e-1,
+    "qpdv" => 1e-1,
+    "qrdiff" => 1e-1,
+    "qediff" => 1e-1,
+    "qeadv" => 1e-1,
+    # Magnetic field: G * 1f-4 T/G = 1f-4 T
+    "bx" => 1e-4,
+    "by" => 1e-4,
+    "bz" => 1e-4,
+    # Electric field: statV/cm * 1f-2 m/cm * 1f-4 T/G * c[cm/s] = 2.998f4 V/m
+    "ex" => 2.99792458e4,
+    "ey" => 2.99792458e4,
+    "ez" => 2.99792458e4,
+    # Temperature: K = K
+    "tg" => 1.0,
+    )
+
  
     @test all([
         BifrostTools.convert_units(data, key, params, "cgs")[1] == value 
@@ -49,7 +84,7 @@ u_B =  1.121e3
     ])
 
     @test all([
-        BifrostTools.convert_units(data, key, params, "si")[1] == value*BifrostTools.cgs_to_SI_conversion_factors[key]
+        BifrostTools.convert_units(data, key, params, "si")[1] == value*test_cgs_to_si_conversions[key]
         for (key, value) in conversions
     ])
 
