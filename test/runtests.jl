1b604beaca3c3c03789b972212c62a048b3f748d
eilifso <e.s.oyre@astro.uio.no>
2024-06-17 12:04:42 +0200

    [ORG] Encapsulate all test in outer testset.

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,9 +10,11 @@ expname = "en48"
 xp = BifrostExperiment(expname,expdir)
 params = read_params(expname,xp.snaps[1],expdir)
 
-include("experiment.jl")
-include("read_params_snap_aux.jl")
-include("stagger_operators.jl")
-include("unit_conversion.jl")
-include("eos_tables.jl")
-include("utils.jl")
+@testset verbose = true "Total" begin
+    include("experiment.jl")
+    include("read_params_snap_aux.jl")
+    include("stagger_operators.jl")
+    include("unit_conversion.jl")
+    include("eos_tables.jl")
+    include("utils.jl")
+end
