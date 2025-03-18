00b0de162546785603f6f7d5fe8ce79f7ff58814
eilifso <e.s.oyre@astro.uio.no>
2025-03-18 10:30:25 +0100

    [DEV] use `isnothing` instead of ` == nothing`.

--- src/experiment.jl
+++ src/experiment.jl
@@ -19,12 +19,12 @@ Fields:
 ***
 """
 struct BifrostExperiment
-    mesh            ::BifrostMesh
-    expname         ::String
-    expdir          ::String
-    snaps           ::Vector{Int64}
-    snapsize        ::Tuple{Int64, Int64, Int64}
-    num_snaps       ::Int64
+    mesh::BifrostMesh
+    expname::String
+    expdir::String
+    snaps::Vector{Int64}
+    snapsize::Tuple{Int64,Int64,Int64}
+    num_snaps::Int64
     num_primary_vars::Int64
 
     function BifrostExperiment(
@@ -32,31 +32,31 @@ struct BifrostExperiment
         expdir::String=pwd()
         ;
         mesh_file=nothing
-        )
-        if expname=="none"
+    )
+        if expname == "none"
             expname = splitpath(expdir)[end]
         end
 
         filenames = readdir(expdir)
         # Find mesh-file if not given.
-        if mesh_file == nothing
+        if isnothing(mesh_file)
             mesh_file = ""
             mesh_match = false
             for filename in filenames
                 match_result = match(r"^" * expname * r".*\.mesh$", filename)
-                if match_result != nothing
+                if !isnothing(match_result)
                     if mesh_match
                         error("Found multiple mesh files.")
                     else
                         mesh_file *= match_result.match
-                        mesh_match = true 
+                        mesh_match = true
                     end
                 end
             end
         else
             mesh_match = true
         end
-        if mesh_match 
+        if mesh_match
             mesh = BifrostMesh(string(expdir, "/", mesh_file))
         else
             error("Did not find mesh file with expname '$expname' in $expdir")
@@ -64,12 +64,12 @@ struct BifrostExperiment
 
         # Find number of snaps
         snaps = get_snap_numbers(expdir, expname; filenames=filenames,
-                                    findall=true)
+            findall=true)
 
         # Get some snap-independent parameters
         params_file = string(
-            joinpath(expdir,expname),
-            "_", lpad(snaps[1],3,"0"),
+            joinpath(expdir, expname),
+            "_", lpad(snaps[1], 3, "0"),
             ".idl"
         )
         params = read_params(params_file)
@@ -78,7 +78,7 @@ struct BifrostExperiment
         # Create struct-instace.
         new(mesh, expname, expdir, sort(snaps), snapsize, length(snaps),
             num_vars[1]
-            )
+        )
     end
 end
 
@@ -96,7 +96,7 @@ function get_axes(
     xp::BifrostExperiment
     ;
     units="code"
-    )
+)
     params = read_params(xp, xp.snaps[1])
     return convert_axesunits(xp.mesh, params; units=units)
 end
